519f1a42c50212b40ec3a29f30f9a87bab62b50e
Khian Calderon <ckhianvictory@gmail.com>
2025-05-13 21:20:18 +0800

    Updated Basic.py

--- basic.py
+++ basic.py
@@ -4,8 +4,11 @@ ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("blue")
 
 app = ctk.CTk()
+
+screen_width = app.winfo_screenwidth()
+screen_height = app.winfo_screenheight()
+app.geometry(f"{screen_width}x{screen_height}+0+0")
+
 app.update()
 app.state("zoomed")
-app.title("Sample Maxmized Window")
-
 app.mainloop()
